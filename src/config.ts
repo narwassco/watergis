cfe8ff5f535599b08d0fa123f94171cf0a30181f
Jin Igarashi <igarashi.jin@water-gis.com>
2022-06-13 21:20:09 +0300

    disabled terrain mode

--- src/config.ts
+++ src/config.ts
@@ -153,9 +153,9 @@ export const config: Config = {
 				}
 			]
 		}
-	},
-	terrain: {
-		source: 'narok-dem',
-		exaggeration: 1
 	}
+	// terrain: {
+	// 	source: 'narok-dem',
+	// 	exaggeration: 1
+	// }
 };
