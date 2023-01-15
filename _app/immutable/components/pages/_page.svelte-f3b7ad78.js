6b5c25e4ab759e729d843f101e14bbe2fedb9ab3
JinIgarashi <igarashi.jin@water-gis.com>
2023-01-15 17:30:44 +0000

    Deploying to gh-pages from @ narwassco/watergis@0ecfe0f9567371b8bdeb8b1bfa2c09b19dbfb693 🚀

---
+++ _app/immutable/components/pages/_page.svelte-f3b7ad78.js
@@ -0,0 +1,1 @@
+import"../../chunks/preload-helper-41c905a7.js";import"../../chunks/index-6a020e87.js";import{P as p}from"../../chunks/_page-6f66caea.js";export{p as default};
--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const m = [
   "/_app/immutable/assets/_page-2fe65346.css",
+  "/_app/immutable/components/pages/_page.svelte-f3b7ad78.js",
   "/_app/immutable/chunks/preload-helper-41c905a7.js",
-  "/_app/immutable/chunks/index-95d721c5.js",
-  "/_app/immutable/chunks/singletons-d7293337.js",
-  "/_app/immutable/chunks/index-d23b2ecc.js",
-  "/_app/immutable/chunks/index-71eac763.js",
-  "/_app/immutable/chunks/2-ce4eb8ed.js",
-  "/_app/immutable/chunks/_page-7e4796dc.js",
+  "/_app/immutable/chunks/index-b7e32f82.js",
   "/_app/immutable/chunks/purify.es-075a2866.js",
-  "/_app/immutable/start-9a10c772.js",
-  "/_app/immutable/chunks/1-8d9436e2.js",
-  "/_app/immutable/chunks/0-016a1d45.js",
-  "/_app/immutable/components/pages/_page.svelte-ca0acc90.js",
-  "/_app/immutable/components/error.svelte-1d9f4709.js",
+  "/_app/immutable/components/error.svelte-9a383711.js",
+  "/_app/immutable/chunks/1-8961486a.js",
+  "/_app/immutable/chunks/2-4f997284.js",
+  "/_app/immutable/chunks/_page-7e4796dc.js",
+  "/_app/immutable/components/layout.svelte-ccac2896.js",
   "/_app/immutable/modules/pages/_page.ts-6a8cdd0a.js",
+  "/_app/immutable/chunks/index-6a020e87.js",
+  "/_app/immutable/chunks/singletons-9256956e.js",
+  "/_app/immutable/chunks/0-e90cdcb6.js",
+  "/_app/immutable/chunks/index-95d721c5.js",
+  "/_app/immutable/start-910b55c8.js",
   "/_app/immutable/chunks/html2canvas.esm-0a33ed42.js",
-  "/_app/immutable/components/layout.svelte-99529342.js",
-  "/_app/immutable/chunks/index.es-4b58285d.js",
-  "/_app/immutable/chunks/_page-f9c18b1f.js"
+  "/_app/immutable/chunks/index.es-e470cc8e.js",
+  "/_app/immutable/chunks/_page-6f66caea.js"
 ], h = [
   "/.nojekyll",
   "/assets/icons/icon-128x128.png",
@@ -33,40 +33,40 @@ const m = [
   "/favicon.png",
   "/manifest.webmanifest",
   "/robots.txt"
-], i = "1672062400176", a = self, o = `cache${i}`, p = m.concat(h), u = new Set(p);
-a.addEventListener("install", (s) => {
+], i = "1673803787808", t = self, o = `cache${i}`, p = m.concat(h), u = new Set(p);
+t.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(o).then((e) => e.addAll(p)).then(() => {
-      a.skipWaiting();
+      t.skipWaiting();
     })
   );
 });
-a.addEventListener("activate", (s) => {
+t.addEventListener("activate", (s) => {
   s.waitUntil(
     caches.keys().then(async (e) => {
-      for (const t of e)
-        t !== o && await caches.delete(t);
-      a.clients.claim();
+      for (const a of e)
+        a !== o && await caches.delete(a);
+      t.clients.claim();
     })
   );
 });
 async function r(s) {
   const e = await caches.open(`offline${i}`);
   try {
-    const t = await fetch(s);
-    return e.put(s, t.clone()), t;
-  } catch (t) {
+    const a = await fetch(s);
+    return e.put(s, a.clone()), a;
+  } catch (a) {
     const c = await e.match(s);
     if (c)
       return c;
-    throw t;
+    throw a;
   }
 }
-a.addEventListener("fetch", (s) => {
+t.addEventListener("fetch", (s) => {
   if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const e = new URL(s.request.url), t = e.protocol.startsWith("http"), c = e.hostname === self.location.hostname && e.port !== self.location.port, n = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !n;
-  t && !c && !l && s.respondWith(
+  const e = new URL(s.request.url), a = e.protocol.startsWith("http"), c = e.hostname === self.location.hostname && e.port !== self.location.port, n = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !n;
+  a && !c && !l && s.respondWith(
     (async () => n && await caches.match(s.request) || r(s.request))()
   );
 });
